fce011617b7f8c9cc1394a8512c0f5f116427509
Marre3 <marre244@gmail.com>
2022-10-06 23:15:58 +0200

    Add basic input prototype

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env Python3
 
-print("Welcome to vibe a.k.a. vi Barebones Editor")
-# TBD
+import sys
+import tty
+import termios
+
+
+def exit():
+    print("Exiting...")
+    fd = sys.stdin.fileno()
+    old = termios.tcgetattr(fd)
+    old[3] = old[3] | termios.ECHO
+    termios.tcsetattr(fd, termios.TCSADRAIN, old)
+    sys.exit(0)
+
+def main():
+    tty.setcbreak(sys.stdin.fileno())
+
+    print("Welcome to vibe a.k.a. vi Barebones Editor")
+    while True:
+        try:
+            x = sys.stdin.read(1)
+        except KeyboardInterrupt:
+            exit()
+        else:
+            print(x, end="")
+            sys.stdout.flush()
+
+if __name__ == "__main__":
+    main()
+    exit()
